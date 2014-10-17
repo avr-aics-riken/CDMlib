81228b1c94e72e30b90839a51235f49a097ac258
tanaka-advance <tanaka@advancesoft.jp>
2014-10-17 11:11:15 +0900

    Restore SPH format

--- src/cdm_DFI.C
+++ src/cdm_DFI.C
@@ -180,10 +180,10 @@ cdm_DFI* cdm_DFI::ReadInit(const MPI_Comm comm,
   /** dfiのインスタンス **/
   cdm_DFI *dfi = NULL;
   // CIOlibではSPHをサポートしていたが、CDMlibではSPHはサポートしない。
-  /*if( F_info.FileFormat == CDM::E_CDM_FMT_SPH ) {
+  // ↓SPHを復活させた(2014.10.17)
+  if( F_info.FileFormat == CDM::E_CDM_FMT_SPH ) {
     dfi = new cdm_DFI_SPH(F_info, F_path, unit, domain, mpi, TimeSlice, process);
-  } else*/
-  if( F_info.FileFormat == CDM::E_CDM_FMT_BOV ) {
+  } else if( F_info.FileFormat == CDM::E_CDM_FMT_BOV ) {
     dfi = new cdm_DFI_BOV(F_info, F_path, unit, domain, mpi, TimeSlice, process);
   } else if( F_info.FileFormat == CDM::E_CDM_FMT_PLOT3D ) {
     dfi = new cdm_DFI_PLOT3D(F_info, F_path, unit, domain, mpi, TimeSlice, process);
